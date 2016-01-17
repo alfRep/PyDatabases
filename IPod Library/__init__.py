f20e3ac9599c9fdb5a4880d2029be969485bacb0
alfRep <alf.email@gmail.com>
2016-01-16 23:34:40 -0600

    Added roster files

---
+++ IPod Library/__init__.py
@@ -0,0 +1,7 @@
+#  __init__.py.py
+#  PyDatabases
+#  
+#  Created by @author 'ALF' on 16 Jan 2016
+#  Copyright(c) 2016.  All Rights Reserved.
+#-------------------------------------------------------
+import
